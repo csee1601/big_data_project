200478db3e01c1f5168fd690f05b926068c7d35d
vinstarry <vinstarry@outlook.com>
2018-12-25 10:53:29 +0800

    reduce.py bug fixed

--- sort_py/reduce.py
+++ sort_py/reduce.py
@@ -6,16 +6,15 @@ for line in sys.stdin:
     data = line.strip().split(',')
     data_list.append(data)
 
-#print(data_list)
-
 for item in data_list:
     prov = item[0]
-    sales = item[1]
+    sales = float(item[1])
     if prov in result_dict.keys():
-        result_dict[prov] += sales
+        result_dict[prov] += float(sales)
     else:
         result_dict[prov] = sales
 
 final_list = sorted(result_dict.items(), key=lambda item:item[1], reverse=True)
 for item in final_list:
     print(item[0] + ',' + str(item[1]))
+
