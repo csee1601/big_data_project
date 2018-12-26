3f77daaad1b5b7a5f3f3b420ae289a538a863ead
vinstarry <vinstarry@outlook.com>
2018-12-26 19:12:18 +0800

    Visualization with matplotlib.py finished

--- kmeans_py/visualization.py
+++ kmeans_py/visualization.py
@@ -1,13 +1,34 @@
 import sys
+import matplotlib.pyplot as plt
 
 data_list = []
+x_list = []
+y_list = []
+c_list = []
 
-for line in sys.stdin:
-    data = line.strip().split(',')
-    data[0] = int(data[0])
-    data[1] = float(data[1])
-    data[-1] = int(data[-1])
-    data_list.append(data)
+def phrase_input():
+    for line in sys.stdin:
+        data = line.strip().split(',')
+        color_list = ['r', 'y', 'k', 'g', 'm']
+        data[0] = int(data[0])
+        data[1] = float(data[1])
+        data[-1] = int(data[-1])
+        x_list.append(data[0])
+        y_list.append(data[1])
+        c_list.append(color_list[data[-1] % 5])
+        data_list.append(data)
+
+def visualization(x_min = 0, x_max = 8000, y_min = 0, y_max = 30, cluster_num = 3):
+    global data_list
+    global x_list
+    global y_list
+    global c_list
+    plt.xlim(x_max, x_min)
+    plt.ylim(y_max, y_min)
+    plt.title("Clustering Result")
+    plt.scatter(x_list, y_list, c = c_list)
+    plt.show()
 
 if __name__ == '__main__':
-    print(data_list)
+    phrase_input()
+    visualization(cluster_num = 3)
